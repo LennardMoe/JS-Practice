84514ef22940a301a4bacb52f85623c956b4f748
Lennard Moe <103784266+LennardMoe@users.noreply.github.com>
2022-06-30 09:15:52 +0200

    Project 13 - Video Trailer popup

---
+++ Project 13 Video Trailer Popup/main.js
@@ -0,0 +1,15 @@
+const btnEl = document.querySelector(".btn")
+const closedIconEl = document.querySelector(".close-icon")
+const trailerContainerEl = document.querySelector(".trailer-container")
+const videoEl = document.querySelector("video")
+
+
+btnEl.addEventListener("click", () =>{
+    trailerContainerEl.classList.remove("active")
+})
+
+closedIconEl.addEventListener("click", () =>{
+    trailerContainerEl.classList.add("active")
+    videoEl.pause();
+    videoEl.currentTime = 0;
+})
